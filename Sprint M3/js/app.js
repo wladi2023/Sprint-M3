42ccd5d808fc3328ac4d296e77064b8bc35a9675
Wladislaw <123814173+wladi2023@users.noreply.github.com>
2023-02-21 21:14:02 -0300

    Add files via upload

---
+++ Sprint M3/js/app.js
@@ -0,0 +1,16 @@
+let miBoton = document.getElementById("miBoton");
+
+miBoton.addEventListener("click", saludar);
+function saludar(){
+    alert("Hola, todos los días lunes recibirás nuestras ofertas");
+}
+miBoton.addEventListener("click", miFuncion);
+function miFuncion(){
+    alert('¡Muchas gracias!');
+}
+alert("¡Bienvenidos a Cachureando!");
+
+// miBoton.onclick = saludar;
+// miBoton.onclick = despedir;
+
+//evento addEcentListener()
